ae84a7018111e8d940422e2edbd0edb1542dad25
amarachiperpetua8 <amarachiperpetua8@gmail.com>
2025-08-15 05:00:40 -0700

    SetUp DB enviroment

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -2,7 +2,17 @@ import { MongoClient } from "mongodb";
 
 const connectionString = process.env.ATLAS_URI || "";
 
-const client = new MongoClient(connectionString);
+let uri;
+if (process.env.NODE_ENV === "production") {
+  uri = process.env.ATLAS_URI;
+} else if (process.env.NODE_ENV === "staging") {
+  uri = process.env.ATLAS_URI_STAGING;
+} else {
+  // Fallback for local development
+  uri = process.env.ATLAS_URI_LOCAL;
+}
+const client = new MongoClient(uri);
+//const client = new MongoClient(connectionString);
 
 let conn;
 try {
@@ -11,6 +21,14 @@ try {
   console.error(e);
 }
 
-let db = conn.db("sample_training");
+//let db = conn.db("sample_training");
+let db;
+if (process.env.NODE_ENV === "production") {
+  db = conn.db("production_db_name");
+} else if (process.env.NODE_ENV === "staging") {
+  db = conn.db("staging_db_name");
+} else {
+  db = conn.db("local_db_name");
+}
 
 export default db;
